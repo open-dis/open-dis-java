open-dis/open-dis-java

commit f3a1691e90fb1dcad28d04070b98f9036bf45d46

Added javadoc dir, bumped version number to 16 in ant file

src/main/java/edu/nps/moves/dis/Marking.java
@@ -45,13 +45,56 @@ public short getCharacterSet()
 { return characterSet; 
 }
 
+/**
+* Ensure what is set does not go over 11 characters, and anything under
+* 11 characters zero-fills.  post-processing patch
+* @param pCharacters an array of characters to set
+*/
 public void setCharacters(byte[] pCharacters)
-{ characters = pCharacters;
+{
+ if (pCharacters.length >= characters.length)
+   {
+       System.arraycopy(pCharacters, 0, characters, 0, characters.length);
+   }
+ else
+ {
+   int pCharactersLength = pCharacters.length;
+   System.arraycopy(pCharacters, 0, characters, 0, pCharactersLength);
+   for (int ix = pCharactersLength; ix < characters.length; ix++)
+   {
+       // Ensure all zeros in unfilled fields
+       characters[ix] = 0;
+   }
+ }
 }
 
-public byte[] getCharacters()
-{ return characters; }
+/**
+ * An added conveniece method (added by patch): accepts a string, and either
+ * truncates or zero-fills it to fit into the 11-byte character marking field.
+ * @param marking the marking string, converted internally into a character array that
+ * is exactly 11 bytes long
+ */
+public void setCharactersString(String marking)
+{
+  byte[] buff = marking.getBytes();
+  this.setCharacters(buff);
+}
 
+/**
+ * Post-processing added convenience method. Converts the byte array of
+ * characters to a string. This uses the platform's default charset,
+ * rather than respecting the charset specified in the other field. 
+ * For the most part this will work, unless you're in some wacky foreign
+ * country, in which case you should start speaking English.
+ * 
+ * @return character array converted to a string
+ */
+public String getCharactersString()
+{
+    String charString = new String(characters);
+    
+    return charString;
+}
 
 public void marshal(DataOutputStream dos)
 {

commit faac44055007cadceb6c089fe7b12894518a91ee

Fix marking test
Broke in f3a1691e90fb1dcad28d04070b98f9036bf45d46

Returned length is always 11 now. A zero padded string.

And I think it makes sense to keep Marking#getCharacters to avoid breakage
of users of the library.

src/main/java/edu/nps/moves/dis/Marking.java
@@ -68,6 +68,9 @@ public void setCharacters(byte[] pCharacters)
  }
 }
 
+public byte[] getCharacters()
+{ return characters; }
+
 /**
  * An added conveniece method (added by patch): accepts a string, and either
  * truncates or zero-fills it to fit into the 11-byte character marking field.

src/test/java/edu/nps/moves/dis/MarkingTest.java
@@ -16,6 +16,8 @@ import static org.junit.Assert.*;
  */
 public class MarkingTest
 {
+    private static final int MARKING_STRING_LENGTH = 11;
+    
     public void Marking()
     {
 
@@ -29,7 +31,7 @@ public class MarkingTest
         final String s = new String("This is a marking that is much, much too loong");
         marking.setCharacters(s.getBytes());
         byte[] buff = marking.getCharacters();
-        assertEquals(buff.length, s.length());
+        assertEquals(buff.length, MARKING_STRING_LENGTH);
     }
 
     @Test
@@ -40,7 +42,7 @@ public class MarkingTest
         final String s = new String("short");
         marking.setCharacters(s.getBytes());
         byte[] buff = marking.getCharacters();
-        assertEquals(buff.length, s.length());
+        assertEquals(buff.length, MARKING_STRING_LENGTH);
     }
 
     @Before