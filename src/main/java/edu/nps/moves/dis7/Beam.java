eeff7499b4409470930d4ed0286d0880492d9475
luke.cowan <luke.cowan@bbsr.co.uk>
2020-11-13 10:40:21 +0000

    Created Beam class for ElectromagneticEmissionPdu dis7

---
+++ src/main/java/edu/nps/moves/dis7/Beam.java
@@ -0,0 +1,339 @@
+package edu.nps.moves.dis7;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+/**
+ * This field shall specify information about a particular active beam. Section 7.6.2.
+ *
+ * Copyright (c) 2008-2016, MOVES Institute, Naval Postgraduate School. All rights reserved. This work is licensed under the BSD open source
+ * license, available at https://www.movesinstitute.org/licenses/bsd.html
+ * @author DMcG
+ */
+public class Beam extends Object implements Serializable
+{
+    /** Specifies the length of this beam's data, 8 bit unsigned int */
+    protected short beamDataLength;
+
+    /** unique beam number, 8 bit unsigned int */
+    protected short beamNumber;
+
+    /**
+     * Used in conjunction with the Emitter Name field as a database primary key, this field shall specify a number by which receiving
+     * entities reference stored database parameters required to regenerate the beam., 16 bit unsigned int
+     */
+    protected int beamParameterIndex;
+
+    /**
+     * specify dynamic parameters of the emitter and shall be represented by an EE Fundamental Parameter Data record (see 6.2.22).
+     */
+    protected EEFundamentalParameterData fundamentalParameterData = new EEFundamentalParameterData();
+
+    /**
+     * specify parameters of the beam and shall be represented by a Beam Data record (see 6.2.11)
+     */
+    protected BeamData beamData = new BeamData();
+
+    /**
+     * specify the intended use of a particular beam. Typical functions include search, acquisition, tracking, illumination, jamming, and so
+     * on. This field is intended to help receiving entities determine the emission mode represented by the beam. This field shall be
+     * represented by an 8-bit enumeration
+     */
+    protected short beamFunction;
+
+    /**
+     * This field, in conjunction with the High-Density Track/Jam field, shall identify, for the current PDU and emitter beam, the number of
+     * entities tracked or under illumination (as appropriate for an emitter beam’s function) or the number of targeted emitter beams (for
+     * jammers). This field shall be represented by an 8-bit unsigned integer.
+     */
+    protected short numberOfTargets;
+
+    /**
+     * This field shall be used to indicate that receiving simulation applications can assume that all viable targets in the field of regard
+     * specified by the beam data are being tracked or jammed. This field shall be represented by an 8-bit enumeration
+     */
+    protected short highDensityTrackJam;
+
+    /**
+     * This field shall indicate the status of the beam (e.g., the beam is active or deactivated) and shall be represented by the Beam
+     * Status record (see 6.2.12)
+     */
+    protected BeamStatus beamStatus = new BeamStatus();
+
+    /**
+     * Jamming Technique. This field shall be used to identify the jamming method or methods and shall be represented by a Jamming Technique
+     * record (see 6.2.49).
+     */
+    protected JammingTechnique jammingTechnique = new JammingTechnique();
+
+    /**
+     * This field is optional for any given beam. Rules for inclusion and use are provided in 5.7.3.3, 5.7.3.7, and 5.7.3.8. When included,
+     * this field shall be represented by a series of Track/Jam Data records (see 6.2.90).
+     */
+    protected TrackJamData trackJamData = new TrackJamData();
+
+    /** Constructor */
+    public Beam()
+    {
+    }
+
+    public int getMarshalledSize()
+    {
+        int marshalSize = 0;
+
+        marshalSize = marshalSize + 1;  // beamDataLength
+        marshalSize = marshalSize + 1;  // beamNumber
+        marshalSize = marshalSize + 2;  // beamParameterIndex
+        marshalSize = marshalSize + fundamentalParameterData.getMarshalledSize();  // fundamentalParameterData
+        marshalSize = marshalSize + beamData.getMarshalledSize();  // beamData
+        marshalSize = marshalSize + 1;  // beamFunction
+        marshalSize = marshalSize + 1;  // numberOfTargets
+        marshalSize = marshalSize + 1;  // highDensityTrackJam
+        marshalSize = marshalSize + beamStatus.getMarshalledSize();  // beamStatus
+        marshalSize = marshalSize + jammingTechnique.getMarshalledSize();  // jammingTechnique
+        if (numberOfTargets != 0) {
+            marshalSize = marshalSize + trackJamData.getMarshalledSize();  // trackJamData
+        }
+
+        return marshalSize;
+    }
+
+    public EEFundamentalParameterData getFundamentalParameterData()
+    {
+        return fundamentalParameterData;
+    }
+
+    public void setFundamentalParameterData(EEFundamentalParameterData fundamentalParameterData)
+    {
+        this.fundamentalParameterData = fundamentalParameterData;
+    }
+
+    public BeamData getBeamData()
+    {
+        return beamData;
+    }
+
+    public void setBeamData(BeamData beamData)
+    {
+        this.beamData = beamData;
+    }
+
+    public short getBeamFunction()
+    {
+        return beamFunction;
+    }
+
+    public void setBeamFunction(short beamFunction)
+    {
+        this.beamFunction = beamFunction;
+    }
+
+    public short getNumberOfTargets()
+    {
+        return numberOfTargets;
+    }
+
+    public void setNumberOfTargets(short numberOfTargets)
+    {
+        this.numberOfTargets = numberOfTargets;
+    }
+
+    public short getHighDensityTrackJam()
+    {
+        return highDensityTrackJam;
+    }
+
+    public void setHighDensityTrackJam(short highDensityTrackJam)
+    {
+        this.highDensityTrackJam = highDensityTrackJam;
+    }
+
+    public BeamStatus getBeamStatus()
+    {
+        return beamStatus;
+    }
+
+    public void setBeamStatus(BeamStatus beamStatus)
+    {
+        this.beamStatus = beamStatus;
+    }
+
+    public JammingTechnique getJammingTechnique()
+    {
+        return jammingTechnique;
+    }
+
+    public void setJammingTechnique(JammingTechnique jammingTechnique)
+    {
+        this.jammingTechnique = jammingTechnique;
+    }
+
+    public TrackJamData getTrackJamData()
+    {
+        return trackJamData;
+    }
+
+    public void setTrackJamData(TrackJamData trackJamData)
+    {
+        this.trackJamData = trackJamData;
+    }
+
+    public int getBeamDataLength()
+    {
+        return beamDataLength;
+    }
+
+    public void setBeamDataLength(short beamDataLength)
+    {
+        this.beamDataLength = beamDataLength;
+    }
+
+    public short getBeamNumber()
+    {
+        return beamNumber;
+    }
+
+    public void setBeamNumber(short pEmitterFunction)
+    {
+        beamNumber = pEmitterFunction;
+    }
+
+    public int getBeamParameterIndex()
+    {
+        return beamParameterIndex;
+    }
+
+    public void setBeamParameterIndex(short pEmitterIDNumber)
+    {
+        beamParameterIndex = pEmitterIDNumber;
+    }
+
+    public void marshal(DataOutputStream dos)
+    {
+        try {
+            dos.writeByte((byte) beamDataLength);
+            dos.writeByte((byte) beamNumber);
+            dos.writeShort(beamParameterIndex);
+            fundamentalParameterData.marshal(dos);
+            beamData.marshal(dos);
+            dos.writeByte((byte) beamFunction);
+            dos.writeByte((byte) numberOfTargets);
+            dos.writeByte((byte) highDensityTrackJam);
+            beamStatus.marshal(dos);
+            jammingTechnique.marshal(dos);
+            if (numberOfTargets != 0) { //When the Number of Targets value is zero, this field shall be omitted
+                trackJamData.marshal(dos);
+            }
+        } // end try
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    } // end of marshal method
+
+    /**
+     * Packs a Pdu into the ByteBuffer.
+     * @param buff The ByteBuffer at the position to begin writing
+     * @throws java.nio.BufferOverflowException if buff is too small
+     * @throws java.nio.ReadOnlyBufferException if buff is read only
+     * @see java.nio.ByteBuffer
+     * @since ??
+     */
+    public void marshal(java.nio.ByteBuffer buff)
+    {
+        buff.put((byte) beamDataLength);
+        buff.put((byte) beamNumber);
+        buff.putShort((short) beamParameterIndex);
+        fundamentalParameterData.marshal(buff);
+        beamData.marshal(buff);
+        buff.put((byte) beamFunction);
+        buff.put((byte) numberOfTargets);
+        buff.put((byte) highDensityTrackJam);
+        beamStatus.marshal(buff);
+        jammingTechnique.marshal(buff);
+        if (numberOfTargets != 0) { //When the Number of Targets value is zero, this field shall be omitted
+            trackJamData.marshal(buff);
+        }
+    } // end of marshal method
+
+    public void unmarshal(DataInputStream dis)
+    {
+        try {
+            beamDataLength = (short) dis.readUnsignedByte();
+            beamNumber = (short) dis.readUnsignedByte();
+            beamParameterIndex = dis.readUnsignedShort();
+            fundamentalParameterData.unmarshal(dis);
+            beamData.unmarshal(dis);
+            beamFunction = (short) dis.readUnsignedShort();
+            numberOfTargets = (short) dis.readUnsignedShort();
+            highDensityTrackJam = (short) dis.readUnsignedShort();
+            beamStatus.unmarshal(dis);
+            jammingTechnique.unmarshal(dis);
+            if (numberOfTargets != 0) { //When the Number of Targets value is zero, this field shall be omitted
+                trackJamData.unmarshal(dis);
+            }
+
+        } // end try
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    } // end of unmarshal method
+
+    /**
+     * Unpacks a Pdu from the underlying data.
+     * @param buff The ByteBuffer at the position to begin reading
+     * @throws java.nio.BufferUnderflowException if buff is too small
+     * @see java.nio.ByteBuffer
+     * @since ??
+     */
+    public void unmarshal(java.nio.ByteBuffer buff)
+    {
+        beamDataLength = (short) ( buff.get() & 0xFF );
+        beamNumber = (short) ( buff.get() & 0xFF );
+        beamParameterIndex = ( buff.getShort() & 0xFFFF );
+        fundamentalParameterData.unmarshal(buff);
+        beamData.unmarshal(buff);
+        beamFunction = (short) ( buff.get() & 0xFF );
+        numberOfTargets = (short) ( buff.get() & 0xFF );
+        highDensityTrackJam = (short) ( buff.get() & 0xFF );
+        beamStatus.unmarshal(buff);
+        jammingTechnique.unmarshal(buff);
+        if (numberOfTargets != 0) { //When the Number of Targets value is zero, this field shall be omitted
+            trackJamData.unmarshal(buff);
+        }
+    } // end of unmarshal method
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Beam beam = (Beam) o;
+        return beamDataLength == beam.beamDataLength &&
+                   beamNumber == beam.beamNumber &&
+                   beamParameterIndex == beam.beamParameterIndex &&
+                   beamFunction == beam.beamFunction &&
+                   numberOfTargets == beam.numberOfTargets &&
+                   highDensityTrackJam == beam.highDensityTrackJam &&
+                   Objects.equals(fundamentalParameterData, beam.fundamentalParameterData) &&
+                   Objects.equals(beamData, beam.beamData) &&
+                   Objects.equals(beamStatus, beam.beamStatus) &&
+                   Objects.equals(jammingTechnique, beam.jammingTechnique) &&
+                   Objects.equals(trackJamData, beam.trackJamData);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects
+                   .hash(beamDataLength, beamNumber, beamParameterIndex, fundamentalParameterData, beamData, beamFunction, numberOfTargets, highDensityTrackJam, beamStatus, jammingTechnique, trackJamData);
+    }
+
+} // end of class
---
+++ src/main/java/edu/nps/moves/dis7/PduFactory.java
@@ -0,0 +1,317 @@
+package edu.nps.moves.dis7;
+
+
+import edu.nps.moves.dis7.AcknowledgePdu;
+import edu.nps.moves.dis7.AcknowledgeReliablePdu;
+import edu.nps.moves.dis7.ActionRequestPdu;
+import edu.nps.moves.dis7.ActionRequestReliablePdu;
+import edu.nps.moves.dis7.ActionResponsePdu;
+import edu.nps.moves.dis7.ActionResponseReliablePdu;
+import edu.nps.moves.dis7.ArealObjectStatePdu;
+import edu.nps.moves.dis7.CollisionElasticPdu;
+import edu.nps.moves.dis7.CollisionPdu;
+import edu.nps.moves.dis7.CommentPdu;
+import edu.nps.moves.dis7.CommentReliablePdu;
+import edu.nps.moves.dis7.CreateEntityPdu;
+import edu.nps.moves.dis7.CreateEntityReliablePdu;
+import edu.nps.moves.dis7.DataPdu;
+import edu.nps.moves.dis7.DataQueryPdu;
+import edu.nps.moves.dis7.DataQueryReliablePdu;
+import edu.nps.moves.dis7.DataReliablePdu;
+import edu.nps.moves.dis7.DesignatorPdu;
+import edu.nps.moves.dis7.DetonationPdu;
+import edu.nps.moves.dis7.ElectronicEmissionsPdu;
+import edu.nps.moves.dis7.EntityStatePdu;
+import edu.nps.moves.dis7.EntityStateUpdatePdu;
+import edu.nps.moves.dis7.EventReportPdu;
+import edu.nps.moves.dis7.EventReportReliablePdu;
+import edu.nps.moves.dis7.FastEntityStatePdu;
+import edu.nps.moves.dis7.FirePdu;
+import edu.nps.moves.dis7.IntercomControlPdu;
+import edu.nps.moves.dis7.IntercomSignalPdu;
+import edu.nps.moves.dis7.IsPartOfPdu;
+import edu.nps.moves.dis7.LinearObjectStatePdu;
+import edu.nps.moves.dis7.MinefieldResponseNackPdu;
+import edu.nps.moves.dis7.MinefieldStatePdu;
+import edu.nps.moves.dis7.Pdu;
+import edu.nps.moves.dis7.PointObjectStatePdu;
+import edu.nps.moves.dis7.ReceiverPdu;
+import edu.nps.moves.dis7.RecordQueryReliablePdu;
+import edu.nps.moves.dis7.RemoveEntityPdu;
+import edu.nps.moves.dis7.RemoveEntityReliablePdu;
+import edu.nps.moves.dis7.RepairCompletePdu;
+import edu.nps.moves.dis7.RepairResponsePdu;
+import edu.nps.moves.dis7.ResupplyOfferPdu;
+import edu.nps.moves.dis7.ResupplyReceivedPdu;
+import edu.nps.moves.dis7.SeesPdu;
+import edu.nps.moves.dis7.ServiceRequestPdu;
+import edu.nps.moves.dis7.SetDataPdu;
+import edu.nps.moves.dis7.SetDataReliablePdu;
+import edu.nps.moves.dis7.SignalPdu;
+import edu.nps.moves.dis7.StartResumePdu;
+import edu.nps.moves.dis7.StartResumeReliablePdu;
+import edu.nps.moves.dis7.StopFreezePdu;
+import edu.nps.moves.dis7.StopFreezeReliablePdu;
+import edu.nps.moves.dis7.TransmitterPdu;
+import edu.nps.moves.dis7.UaPdu;
+import edu.nps.moves.disenum.PduType;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class PduFactory
+{
+    private boolean useFastPdu;
+    private Logger logger;
+
+    public PduFactory()
+    {
+        this(false);
+    }
+
+    public PduFactory(boolean useFastPdu)
+    {
+        this.useFastPdu = false;
+        this.useFastPdu = useFastPdu;
+        this.logger = Logger.getLogger(edu.nps.moves.disutil.PduFactory.class.getName());
+        this.logger.setLevel(Level.OFF);
+    }
+
+    public boolean getUseFastPdu()
+    {
+        return this.useFastPdu;
+    }
+
+    public void setUseFastPdu(boolean use)
+    {
+        this.useFastPdu = use;
+    }
+
+    public void setLoggingLevel(Level loggingLevel)
+    {
+        this.logger.setLevel(loggingLevel);
+    }
+
+    public Pdu createPdu(byte[] data)
+    {
+        return this.createPdu(ByteBuffer.wrap(data));
+    }
+
+    public Pdu createPdu(ByteBuffer buff)
+    {
+        int pos = buff.position();
+        if (pos + 2 > buff.limit()) {
+            return null;
+        } else {
+            buff.position(pos + 2);
+            int pduType = buff.get() & 255;
+            buff.position(pos);
+            PduType pduTypeEnum = PduType.lookup[pduType];
+            Pdu aPdu = null;
+            switch (pduTypeEnum) {
+                case ENTITY_STATE:
+                    if (this.useFastPdu) {
+                        aPdu = new FastEntityStatePdu();
+                    } else {
+                        aPdu = new EntityStatePdu();
+                    }
+                    break;
+                case FIRE:
+                    aPdu = new FirePdu();
+                    break;
+                case DETONATION:
+                    aPdu = new DetonationPdu();
+                    break;
+                case COLLISION:
+                    aPdu = new CollisionPdu();
+                    break;
+                case SERVICE_REQUEST:
+                    aPdu = new ServiceRequestPdu();
+                    break;
+                case RESUPPLY_OFFER:
+                    aPdu = new ResupplyOfferPdu();
+                    break;
+                case RESUPPLY_RECEIVED:
+                    aPdu = new ResupplyReceivedPdu();
+                    break;
+                case REPAIR_COMPLETE:
+                    aPdu = new RepairCompletePdu();
+                    break;
+                case REPAIR_RESPONSE:
+                    aPdu = new RepairResponsePdu();
+                    break;
+                case CREATE_ENTITY:
+                    aPdu = new CreateEntityPdu();
+                    break;
+                case REMOVE_ENTITY:
+                    aPdu = new RemoveEntityPdu();
+                    break;
+                case START_RESUME:
+                    aPdu = new StartResumePdu();
+                    break;
+                case STOP_FREEZE:
+                    aPdu = new StopFreezePdu();
+                    break;
+                case ACKNOWLEDGE:
+                    aPdu = new AcknowledgePdu();
+                    break;
+                case ACTION_REQUEST:
+                    aPdu = new ActionRequestPdu();
+                    break;
+                case ACTION_RESPONSE:
+                    aPdu = new ActionResponsePdu();
+                    break;
+                case DATA_QUERY:
+                    aPdu = new DataQueryPdu();
+                    break;
+                case SET_DATA:
+                    aPdu = new SetDataPdu();
+                    break;
+                case DATA:
+                    aPdu = new DataPdu();
+                    break;
+                case EVENT_REPORT:
+                    aPdu = new EventReportPdu();
+                    break;
+                case COMMENT:
+                    aPdu = new CommentPdu();
+                    break;
+                case ELECTROMAGNETIC_EMISSION:
+                    aPdu = new ElectronicEmissionsPdu();
+                    break;
+                case DESIGNATOR:
+                    aPdu = new DesignatorPdu();
+                    break;
+                case TRANSMITTER:
+                    aPdu = new TransmitterPdu();
+                    break;
+                case SIGNAL:
+                    aPdu = new SignalPdu();
+                    break;
+                case RECEIVER:
+                    aPdu = new ReceiverPdu();
+                    break;
+                case UNDERWATER_ACOUSTIC:
+                    aPdu = new UaPdu();
+                    break;
+                case SUPPLEMENTAL_EMISSION_ENTITY_STATE:
+                    aPdu = new SeesPdu();
+                    break;
+                case INTERCOM_SIGNAL:
+                    aPdu = new IntercomSignalPdu();
+                    break;
+                case INTERCOM_CONTROL:
+                    aPdu = new IntercomControlPdu();
+                    break;
+                case ISPARTOF:
+                    aPdu = new IsPartOfPdu();
+                    break;
+                case MINEFIELD_STATE:
+                    aPdu = new MinefieldStatePdu();
+                    break;
+                case MINEFIELD_RESPONSE_NAK:
+                    aPdu = new MinefieldResponseNackPdu();
+                    break;
+                case POINT_OBJECT_STATE:
+                    aPdu = new PointObjectStatePdu();
+                    break;
+                case LINEAR_OBJECT_STATE:
+                    aPdu = new LinearObjectStatePdu();
+                    break;
+                case AREAL_OBJECT_STATE:
+                    aPdu = new ArealObjectStatePdu();
+                    break;
+                case CREATE_ENTITY_R:
+                    aPdu = new CreateEntityReliablePdu();
+                    break;
+                case REMOVE_ENTITY_R:
+                    aPdu = new RemoveEntityReliablePdu();
+                    break;
+                case START_RESUME_R:
+                    aPdu = new StartResumeReliablePdu();
+                    break;
+                case STOP_FREEZE_R:
+                    aPdu = new StopFreezeReliablePdu();
+                    break;
+                case ACKNOWLEDGE_R:
+                    aPdu = new AcknowledgeReliablePdu();
+                    break;
+                case ACTION_REQUEST_R:
+                    aPdu = new ActionRequestReliablePdu();
+                    break;
+                case ACTION_RESPONSE_R:
+                    aPdu = new ActionResponseReliablePdu();
+                    break;
+                case DATA_QUERY_R:
+                    aPdu = new DataQueryReliablePdu();
+                    break;
+                case SET_DATA_R:
+                    aPdu = new SetDataReliablePdu();
+                    break;
+                case DATA_R:
+                    aPdu = new DataReliablePdu();
+                    break;
+                case EVENT_REPORT_R:
+                    aPdu = new EventReportReliablePdu();
+                    break;
+                case COMMENT_R:
+                    aPdu = new CommentReliablePdu();
+                    break;
+                case RECORD_QUERY_R:
+                    aPdu = new RecordQueryReliablePdu();
+                    break;
+                case COLLISION_ELASTIC:
+                    aPdu = new CollisionElasticPdu();
+                    break;
+                case ENTITY_STATE_UPDATE:
+                    aPdu = new EntityStateUpdatePdu();
+                    break;
+                default:
+                    this.logger.log(Level.INFO, "PDU not implemented. Type = " + pduType + "\n");
+                    if (pduTypeEnum != null) {
+                        this.logger.log(Level.INFO, "  PDU  name is: " + pduTypeEnum.getDescription());
+                    }
+            }
+
+            if (aPdu != null) {
+                ( (Pdu) aPdu ).unmarshal(buff);
+            }
+
+            return (Pdu) aPdu;
+        }
+    }
+
+    public List<Pdu> getPdusFromBundle(byte[] data)
+    {
+        ArrayList<Pdu> pdus = new ArrayList();
+        int pduStartPointInData = 0;
+
+        while (true) {
+            byte[] remaining = Arrays.copyOfRange(data, pduStartPointInData, data.length);
+
+            try {
+                Pdu pdu = this.createPdu(remaining);
+                if (pdu == null) {
+                    break;
+                }
+
+                pdus.add(pdu);
+                int pduLength = pdu.getLength();
+                pduStartPointInData += pduLength;
+                if (pduStartPointInData >= data.length) {
+                    break;
+                }
+            } catch (Exception var7) {
+                System.out.println("Problems decoding multiple PDUs in datagram; decoded as may as possible");
+                break;
+            }
+        }
+
+        return pdus;
+    }
+}
