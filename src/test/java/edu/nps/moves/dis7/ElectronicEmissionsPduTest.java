dc2864cd48dd6b28f4ad5c1839d0666bfd165b11
FO <fo@ifad.dk>
2024-02-05 07:18:00 +0100

    edited to be able to have one or more emitter systems in Emission pdu, and multiple track/jam data, for DIS 7

--- src/test/java/edu/nps/moves/dis7/ElectronicEmissionsPduTest.java
+++ src/test/java/edu/nps/moves/dis7/ElectronicEmissionsPduTest.java
@@ -58,7 +58,7 @@ public class ElectronicEmissionsPduTest
         assertEquals(51, pdu.getExerciseID());
         assertEquals(23, pdu.getPduType());
         assertEquals(6, pdu.getProtocolFamily());
-        assertEquals(100, pdu.getMarshalledSize());
+        assertEquals(104, pdu.getMarshalledSize());
         assertEquals(0, pdu.getPadding());
 
         ElectronicEmissionsPdu espdu = (ElectronicEmissionsPdu) pdu;
@@ -72,41 +72,41 @@ public class ElectronicEmissionsPduTest
         assertEquals(0, espdu.getEventID().getEventNumber());
 
         // Emission System
-        assertEquals(18, espdu.getSystemDataLength());
-        assertEquals(1, espdu.getNumberOfBeams());
+        assertEquals(18, espdu.getSystems().get(0).getSystemDataLength());
+        assertEquals(1, espdu.getSystems().get(0).getNumberOfBeams());
 
         // Emitter System
-        assertEquals(15660, espdu.getEmitterSystem().getEmitterName());
-        assertEquals(7, espdu.getEmitterSystem().getEmitterFunction());
-        assertEquals(1, espdu.getEmitterSystem().getEmitterIDNumber());
+        assertEquals(15660, espdu.getSystems().get(0).getEmitterSystem().getEmitterName());
+        assertEquals(7, espdu.getSystems().get(0).getEmitterSystem().getEmitterFunction());
+        assertEquals(1, espdu.getSystems().get(0).getEmitterSystem().getEmitterIDNumber());
 
         // Location
-        assertEquals(0.0, espdu.getLocation().getX(), 0.0001);
-        assertEquals(0.0, espdu.getLocation().getY(), 0.0001);
-        assertEquals(0.0, espdu.getLocation().getZ(), 0.0001);
+        assertEquals(0.0, espdu.getSystems().get(0).getLocation().getX(), 0.0001);
+        assertEquals(0.0, espdu.getSystems().get(0).getLocation().getY(), 0.0001);
+        assertEquals(0.0, espdu.getSystems().get(0).getLocation().getZ(), 0.0001);
 
         // Beam
-        assertEquals(13, espdu.getElectronicEmissionBeamData().getBeamDataLength());
-        assertEquals(1, espdu.getElectronicEmissionBeamData().getBeamNumber());
-        assertEquals(1, espdu.getElectronicEmissionBeamData().getBeamParameterIndex());
-        assertEquals(14000000000.0, espdu.getElectronicEmissionBeamData().getFundamentalParameterData().getFrequency(), 0.000001);
-        assertEquals(6000000000.0, espdu.getElectronicEmissionBeamData().getFundamentalParameterData().getFrequencyRange(), 0.000001);
-        assertEquals(55.0, espdu.getElectronicEmissionBeamData().getFundamentalParameterData().getEffectiveRadiatedPower(), 0.000001);
-        assertEquals(600.0, espdu.getElectronicEmissionBeamData().getFundamentalParameterData().getPulseRepetitionFrequency(), 0.000001);
-        assertEquals(3.0, espdu.getElectronicEmissionBeamData().getFundamentalParameterData().getPulseWidth(), 0.000001);
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getBeamData().getBeamAzimuthCenter(), 0.000001);
-        assertEquals(0.698132, espdu.getElectronicEmissionBeamData().getBeamData().getBeamAzimuthSweep(), 0.000001);
-        assertEquals(0.436332, espdu.getElectronicEmissionBeamData().getBeamData().getBeamElevationCenter(), 0.000001);
-        assertEquals(0.436332, espdu.getElectronicEmissionBeamData().getBeamData().getBeamElevationSweep(), 0.000001);
-        assertEquals(90.4001, espdu.getElectronicEmissionBeamData().getBeamData().getBeamSweepSync(), 0.000001);
-        assertEquals(7, espdu.getElectronicEmissionBeamData().getBeamFunction());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getNumberOfTargets());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getHighDensityTrackJam());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getBeamStatus().getBeamState());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getJammingTechnique().getCategory());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getJammingTechnique().getKind());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getJammingTechnique().getSpecific());
-        assertEquals(0, espdu.getElectronicEmissionBeamData().getJammingTechnique().getCategory());
+        assertEquals(13, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamDataLength());
+        assertEquals(1, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamNumber());
+        assertEquals(1, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamParameterIndex());
+        assertEquals(14000000000.0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getFundamentalParameterData().getFrequency(), 0.000001);
+        assertEquals(6000000000.0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getFundamentalParameterData().getFrequencyRange(), 0.000001);
+        assertEquals(55.0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getFundamentalParameterData().getEffectiveRadiatedPower(), 0.000001);
+        assertEquals(600.0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getFundamentalParameterData().getPulseRepetitionFrequency(), 0.000001);
+        assertEquals(3.0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getFundamentalParameterData().getPulseWidth(), 0.000001);
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamData().getBeamAzimuthCenter(), 0.000001);
+        assertEquals(0.698132, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamData().getBeamAzimuthSweep(), 0.000001);
+        assertEquals(0.436332, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamData().getBeamElevationCenter(), 0.000001);
+        assertEquals(0.436332, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamData().getBeamElevationSweep(), 0.000001);
+        assertEquals(90.4001, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamData().getBeamSweepSync(), 0.000001);
+        assertEquals(7, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamFunction());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getNumberOfTargets());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getHighDensityTrackJam());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getBeamStatus().getBeamState());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getJammingTechnique().getCategory());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getJammingTechnique().getKind());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getJammingTechnique().getSpecific());
+        assertEquals(0, espdu.getSystems().get(0).getBeamDataRecords().get(0).getJammingTechnique().getCategory());
 
 
     }
